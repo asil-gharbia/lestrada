582182f8c361981c741c49962c75b12d391bb989
aseel-gha <aseel.gh@anvilerp.com>
2025-01-15 17:09:35 +0400

    perms

--- lestrada/materials/report/supplier_quotation_report/supplier_quotation_report.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, Asil Gharbia and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Supplier Quotation Report"] = {
-	"filters": [
-
-	]
-};
